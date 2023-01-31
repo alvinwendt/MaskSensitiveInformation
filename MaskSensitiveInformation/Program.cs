72a1c970a78830e3e6f3bcb5c7e208193d6aeb2b
alvinwendt <alvinwendt@gmail.com>
2023-01-30 20:37:45 -0600

    Add project files.

---
+++ MaskSensitiveInformation/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace MaskSensitiveInformation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Secret Code");
+            string code = Console.ReadLine();
+
+         
+            if (code.Length > 4)
+            {
+                string v = code[^4..].PadLeft(code.Length, '*');
+                var DisplayText = v;
+                Console.WriteLine($"{DisplayText}");
+                Console.ReadLine();
+            }
+
+           
+        }
+
+     
+        }
+    }
